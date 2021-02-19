9f54543d1374d30e3bc7c6091cee135470e6298f
codingscode <heronnestg@hotmail.com>
2021-02-19 18:42:43 -0300

    git commit "parte 1"

--- 001.js
+++ 001.js
@@ -24,11 +24,28 @@ let objeto2 = {produto: 'Biscoito', preco: 2.5, ano: 2019, produto: 'uva'}
 console.log(objeto1)
 console.log(objeto2)
 
+console.log('--------------------------------------')
+
+const matriz1 = [1, 2, 3]
+const matriz2 = [6, 7, 8]
+
+const matriz3 = [...matriz1, ...matriz2]
+const matriz4 = [matriz1, matriz2]
+
+console.log(matriz1)
+console.log(matriz2)
+console.log(matriz3)
+console.log(matriz4)
 
 
 
 
 
+
+
+
+
+console.log('--------------------------------------')
 
 
 
---
+++ 002.js
@@ -0,0 +1,63 @@
+
+class Area {
+    
+    constructor(comprimento, largura) {
+        this.id = Area.aumentarId()  //
+        this.comprimento = comprimento
+        this.largura = largura
+    }
+
+    get area() {
+       return this.CalcularArea()
+    }
+
+    CalcularArea() {
+        return this.largura * this.comprimento
+    }
+
+    static aumentarId() {
+        if (!this.id) {
+            this.id = 1
+        }
+        else {
+            this.id += 1
+        }
+        return this.id
+    }
+
+}
+
+const area1 = new Area(3, 4)
+const area2 = new Area(5, 8)
+const area3 = new Area(9, 11)
+const area4 = new Area(7, 3.5)
+
+
+
+console.log('area1: ', area1)
+console.log('area1: ', area1.area)
+console.log('area2: ', area2)
+console.log('area2: ', area2.area)
+console.log('area3: ', area3)
+console.log('area3: ', area3.area)
+console.log('area4: ', area4)
+console.log('area4: ', area4.area)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
